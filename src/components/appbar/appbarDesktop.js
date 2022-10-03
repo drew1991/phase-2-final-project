61699f2466c31f395bafb8e8c01cbbbc049af49d
Andrew Onyancha <mr.onyanchaandrew@gmail.com>
2022-10-04 02:50:01 +0300

    list items in appbar

--- src/components/appbar/appbarDesktop.js
+++ src/components/appbar/appbarDesktop.js
@@ -1,3 +1,16 @@
+import { ListItemText } from "@mui/material";
+import { AppBarContainer, AppbarHeader, Mylist } from "../../styles/appbar";
+
 export default function AppbarDesktop({ matches }) {
-  return <h1>Desktop</h1>;
+  return (
+    <AppBarContainer>
+      <AppbarHeader>My Bags</AppbarHeader>
+      <Mylist type="row">
+        <ListItemText primary="home" />
+        <ListItemText primary="Categories" />
+        <ListItemText primary="Products" />
+        <ListItemText primary="Contact Us" />
+      </Mylist>
+    </AppBarContainer>
+  );
 }
--- src/styles/appbar/index.js
+++ src/styles/appbar/index.js
@@ -1,6 +1,29 @@
-import { Box } from "@mui/material";
+import { List, Typography } from "@mui/material";
 import { styled } from "@mui/material";
+import { Box } from "@mui/system";
+import "@fontsource/montez";
+import { Colors } from "../theme";
+// container
+export const AppBarContainer = styled(Box)(() => ({
+  display: "flex",
+  marginTop: 4,
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "2px 8px",
+}));
 
-export const AppBarContainer = styled(Box)(() => {
-  display: "flex";
-});
+// header
+export const AppbarHeader = styled(Typography)(() => ({
+  padding: "4px",
+  flexGrow: 1,
+  fontSize: "4em",
+  fontFamily: '"montez","cursive"',
+  color: Colors.secondary,
+}));
+
+export const Mylist = styled(List)(({ type }) => ({
+  display: type === "row" ? "flex" : "block",
+  flexGrow: 3,
+  justifyContent: "center",
+  alignItems: "center",
+}));
